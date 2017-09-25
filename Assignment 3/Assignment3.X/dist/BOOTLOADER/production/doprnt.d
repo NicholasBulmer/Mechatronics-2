238a2b4770e08f59f28f99d3b68a834071fa1c07
erhmeh <jaminearly@gmail.com>
2017-09-25 23:17:15 +1000

    Map array

--- Assignment 3/Assignment3.X/dist/BOOTLOADER/production/doprnt.d
+++
@@ -1,1 +0,0 @@
- dist/BOOTLOADER/production/doprnt.d  dist/BOOTLOADER/production/doprnt.p1:  C:/Program\ Files\ (x86)/Microchip/xc8/v1.43/sources/common/doprnt.c
--- Assignment 3/Assignment3.X/main.c
+++ Assignment 3/Assignment3.X/main.c
@@ -16,19 +16,43 @@
  * MX2:         Assignment 3
  */
 
-#include <xc.h>
-#include <pic18f67j50.h>
-#include "ProcessorConfig.h"
-#include "ISR.h"
-#include "MXK.h"
-#include "Config.h"
-#include "Functions.h"
-#include "Colours.h"
-#include "Console.h"
+// #include <xc.h>
+// #include <pic18f67j50.h>
+// #include "ProcessorConfig.h"
+// #include "ISR.h"
+// #include "MXK.h"
+// #include "Config.h"
+// #include "Functions.h"
+// #include "Colours.h"
+// #include "Console.h"
 #include <stdio.h>
-#include "Motor.h"
-#include "LCD.h"
-#include "LED.h"
-#include "HMI.h"
-#include "iRobotSerial.h"
-#include "ADC.h"
+// #include "Motor.h"
+// #include "LCD.h"
+// #include "LED.h"
+// #include "HMI.h"
+// #include "iRobotSerial.h"
+// #include "ADC.h"
+
+bool maze[9][11] = {
+        {1,1,1,1,1,1,1,1,1,1,1},
+        {1,0,0,0,1,0,1,0,0,0,1},
+        {1,0,1,1,1,0,1,1,1,0,1},
+        {1,0,0,0,0,0,0,0,0,0,1},
+        {1,1,1,1,1,0,1,1,1,0,1},
+        {1,0,0,0,1,0,1,0,1,0,1},
+        {1,0,1,0,1,0,1,1,1,0,1},
+        {1,0,1,0,0,0,0,0,0,0,1},
+        {1,1,1,1,1,1,1,1,1,1,1},
+};
+
+int bearing = 2;
+int xPos = 9;
+int yPos = 2;
+
+void rotateCW();
+void rotateCCW();
+void moveStraight();
+
+void main(){
+
+}
