c745a0c5494371667f892a8bbee6a91f63e8af17
erhmeh <jaminearly@gmail.com>
2017-09-25 15:08:40 +1000

    Renaming

---
+++ Assignment 3/Assignment3.X/main.c
@@ -0,0 +1,34 @@
+/* TODO:
+ * Please use FUNCTIONS. Makes life so much easier for everyone.
+ * Please comment your functions too as well as the indivudual lines of code.
+ * This is just good practice.
+ */
+
+/*
+ * File:        main.c
+ *
+ * Author(s):   Chad Aryana
+ *              Jamin Early
+ *              Matt Woods
+ *              Nick Bulmer
+ *              Tania Nassir
+ *
+ * MX2:         Assignment 2
+ */
+
+#include <xc.h>
+#include <pic18f67j50.h>
+#include "ProcessorConfig.h"
+#include "ISR.h"
+#include "MXK.h"
+#include "Config.h"
+#include "Functions.h"
+#include "Colours.h"
+#include "Console.h"
+#include <stdio.h>
+#include "Motor.h"
+#include "LCD.h"
+#include "LED.h"
+#include "HMI.h"
+#include "iRobotSerial.h"
+#include "ADC.h"
