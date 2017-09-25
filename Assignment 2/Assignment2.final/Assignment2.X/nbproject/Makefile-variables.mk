7b6ac4006b0d7cf512a2880f9c96dd95bd10f23a
erhmeh <jaminearly@gmail.com>
2017-09-25 15:04:34 +1000

    Commenting and Assignment 3 folder

---
+++ Assignment 2/Assignment2.final/Assignment2.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# NORMAL configuration
+CND_ARTIFACT_DIR_NORMAL=dist/NORMAL/production
+CND_ARTIFACT_NAME_NORMAL=Assignment2.X.production.hex
+CND_ARTIFACT_PATH_NORMAL=dist/NORMAL/production/Assignment2.X.production.hex
+CND_PACKAGE_DIR_NORMAL=${CND_DISTDIR}/NORMAL/package
+CND_PACKAGE_NAME_NORMAL=assignment2.x.tar
+CND_PACKAGE_PATH_NORMAL=${CND_DISTDIR}/NORMAL/package/assignment2.x.tar
+# BOOTLOADER configuration
+CND_ARTIFACT_DIR_BOOTLOADER=dist/BOOTLOADER/production
+CND_ARTIFACT_NAME_BOOTLOADER=Assignment2.X.production.hex
+CND_ARTIFACT_PATH_BOOTLOADER=dist/BOOTLOADER/production/Assignment2.X.production.hex
+CND_PACKAGE_DIR_BOOTLOADER=${CND_DISTDIR}/BOOTLOADER/package
+CND_PACKAGE_NAME_BOOTLOADER=assignment2.x.tar
+CND_PACKAGE_PATH_BOOTLOADER=${CND_DISTDIR}/BOOTLOADER/package/assignment2.x.tar
