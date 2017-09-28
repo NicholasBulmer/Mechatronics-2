a59b0d96e16a3ee02232955f509983e715a58b2a
erhmeh <jaminearly@gmail.com>
2017-09-28 11:03:30 +1000

    Formatting and spelling
    For some reason tania's name was spelt 'tanya'.
    Matt also made it look ugly so i fixed the formatting

--- Assignment 3/Assignment3.X/main.c
+++ Assignment 3/Assignment3.X/main.c
@@ -4,18 +4,18 @@
  * This is just good practice.
  */
 
-/* 
+/*
  * File:        main.c
- * 
+ *
  * Author(s):   Chad Aryana
  *              Jamin Early
  *              Matt Woods
  *              Nick Bulmer
- *              Tanya Nassir
+ *              Tania Nassir
  *
- * MX2:         Assignment 2
+ * MX2:         Assignment 3
  */
- 
+
 #include <xc.h>
 #include <pic18f67j50.h>
 
@@ -51,7 +51,7 @@
 
 int maze[9][11] = {
         {155,155,155,155,155,155,155,155,155,155,155},
-        {155,154,154,154,155,154,155,0,154,154,155},
+        {155,154,154,154,155,154,155, 0 ,154,154,155},
         {155,154,155,155,155,154,155,155,155,154,155},
         {155,154,154,154,154,154,154,154,154,154,155},
         {155,155,155,155,155,154,155,155,155,154,155},
@@ -70,14 +70,14 @@ Motor Stepper;
 extern ADC ADC_AN0;
 
 extern INT16
-	iRDistance,																	//Distance in mm since last call/update
-	distanceTotal,
-	iRAngle,																	//Angle in degrees since last call/update
-	angleTotal;
+        iRDistance,                           //Distance in mm since last call/update
+        distanceTotal,
+        iRAngle,                            //Angle in degrees since last call/update
+        angleTotal;
 
-extern UINT8 
-	iRBumpDrop,
-	iRVirtualWall;
+extern UINT8
+        iRBumpDrop,
+        iRVirtualWall;
 
 INT16 IRValue;
 INT16 localSensorAngle = 0;
@@ -128,176 +128,179 @@ void init();
 ////////////////////////////////////////////////////////////////////////////////
 
 void update_IR_distance(void){
-    extern ADC ADC_AN0;
-    float IRCumulative = 0;
-    UINT8 IRSamples = 20; //20 samples
-	for (UINT16 i = 0; i <= IRSamples; i++){
-		ADC_Start(&ADC_AN0);
-		IRCumulative += (590 / ADC_Voltage(&ADC_AN0)); //get val in mm
-	}
-    //returns millimeters
-    IRValue = ((int)IRCumulative)/IRSamples;
+        extern ADC ADC_AN0;
+        float IRCumulative = 0;
+        UINT8 IRSamples = 20; //20 samples
+        for (UINT16 i = 0; i <= IRSamples; i++) {
+                ADC_Start(&ADC_AN0);
+                IRCumulative += (590 / ADC_Voltage(&ADC_AN0)); //get val in mm
+        }
+        //returns millimeters
+        IRValue = ((int)IRCumulative)/IRSamples;
 }
 
 //Use millimetres and use this as an input parameter for correct_distance()
 UINT16 get_apparentDistance(UINT16 distanceDesired){
-	return (distanceDesired/(cos((90-ANGLE_WALL_FOLLOW)*(M_PI/180)))); 
+        return (distanceDesired/(cos((90-ANGLE_WALL_FOLLOW)*(M_PI/180))));
 }
 
 //This function will move the robot indefinitely until irobot_stop_motion(0);
 void follow_wall(INT16 followSpeed, UINT16 apparentDistance){
-    UINT16 i, _d1=apparentDistance-80, _d2=apparentDistance-30, _d3=apparentDistance+30;  
-    
-    //Check if the stepper has checked that there is a wall to actually follow
-    if (!stepperMoved){
-        move_stepper_neutral();
-        if (rightWall){
-            move_stepper_to(ANGLE_WALL_FOLLOW);
+        UINT16 i, _d1=apparentDistance-80, _d2=apparentDistance-30, _d3=apparentDistance+30;
+
+        //Check if the stepper has checked that there is a wall to actually follow
+        if (!stepperMoved) {
+                move_stepper_neutral();
+                if (rightWall) {
+                        move_stepper_to(ANGLE_WALL_FOLLOW);
+                }
+                else{
+                        move_stepper_to(-ANGLE_WALL_FOLLOW);
+                }
+                stepperMoved = 1;
         }
-        else{
-            move_stepper_to(-ANGLE_WALL_FOLLOW);
-        }
-        stepperMoved = 1;
-    }
-    
-    update_IR_distance();
-    
-    //Bang-bang in all its glory
-    (rightWall) ? (i = -1) : (i = 1);
-    if 		(IRValue < _d1)	{irobot_move(0, followSpeed, -500*i);} 	//Very close                        
-    else if (IRValue < _d2)	{irobot_move(0, followSpeed, -1000*i);}	//Close
-    else if (IRValue > _d3)	{irobot_move(0, followSpeed, 750*i);} 	//Slightly far away
-    else	{irobot_move(0, followSpeed, 1000*i);}                  //Default condition
-    
-	//If the walls disappears while moving, use the other wall as a reference
-	if (IRValue > DISTANCE_NO_WALL){rightWall = !rightWall; stepperMoved = 0;}
+
+        update_IR_distance();
+
+        //Bang-bang in all its glory
+        (rightWall) ? (i = -1) : (i = 1);
+        if    (IRValue < _d1) {irobot_move(0, followSpeed, -500*i);}//Very close
+        else if (IRValue < _d2) {irobot_move(0, followSpeed, -1000*i);} //Close
+        else if (IRValue > _d3) {irobot_move(0, followSpeed, 750*i);} //Slightly far away
+        else  {irobot_move(0, followSpeed, 1000*i);}              //Default condition
+
+        //If the walls disappears while moving, use the other wall as a reference
+        if (IRValue > DISTANCE_NO_WALL) {rightWall = !rightWall; stepperMoved = 0;}
 }
 
 //Checks to see if there is a wall directly infront of it, if there is it moves 500mm away from it
 void irobot_centralise(void){
-    INT16 differenceDistance;
-    update_IR_distance();
-    if (IRValue < 800){
-    differenceDistance = IRValue - 500;
-    (differenceDistance > 0) ? 
-    irobot_move_to_straight(differenceDistance, DEFAULT_SPEED) :
-    irobot_move_to_straight(differenceDistance, -DEFAULT_SPEED);
-    }
+        INT16 differenceDistance;
+        update_IR_distance();
+        if (IRValue < 800) {
+                differenceDistance = IRValue - 500;
+                (differenceDistance > 0) ?
+                irobot_move_to_straight(differenceDistance, DEFAULT_SPEED) :
+                irobot_move_to_straight(differenceDistance, -DEFAULT_SPEED);
+        }
 }
 
 //update the LCD with possible debug info
 void update_display1(void){
-	if (MXK_SwitchTo(eMXK_HMI)) {
-	printf("%c", ENDOFTEXT);
-	printf("Var1: %d\n", angleTotal);
-	Console_Render();
-	if (MXK_Release())
-		MXK_Dequeue();}
+        if (MXK_SwitchTo(eMXK_HMI)) {
+                printf("%c", ENDOFTEXT);
+                printf("Var1: %d\n", angleTotal);
+                Console_Render();
+                if (MXK_Release())
+                        MXK_Dequeue();
+        }
 }
 
 //Send digits to the SSD
 void update_SSD(INT16 renderSSD){
-	if (MXK_SwitchTo(eMXK_HMI)) {
-    HMI_SetNumber(renderSSD);
-    HMI_Render();
-	if (MXK_Release())
-		MXK_Dequeue();}
-    return;
+        if (MXK_SwitchTo(eMXK_HMI)) {
+                HMI_SetNumber(renderSSD);
+                HMI_Render();
+                if (MXK_Release())
+                        MXK_Dequeue();
+        }
+        return;
 }
 
 //convert degrees to a step amount for the irobot CHECK DEFINITIONS FOR STEP VARIABLE
 INT16 degrees_to_steps(INT16 stepperDegrees){
-    return (int)((INVERSE_STEP_AMOUNT*stepperDegrees)/1.8);
+        return (int)((INVERSE_STEP_AMOUNT*stepperDegrees)/1.8);
 }
 
 //Move stepper in DEGREES: -ve is CCW rotation, +ve is CW rotation
 void move_stepper_to(INT16 moveStepperTo){
-    localSensorAngle += moveStepperTo;
-    if (localSensorAngle >= 360 || localSensorAngle <= -360){localSensorAngle = 0;}
-        
-	if (MXK_BlockSwitchTo(eMXK_Motor)) {
-		Motor_Move(&Stepper, degrees_to_steps(moveStepperTo));
-		if (MXK_Release())
-		MXK_Dequeue();} 
-    //wait for the stepper to get to its position before calling something else
-    delay_ms(75);   
+        localSensorAngle += moveStepperTo;
+        if (localSensorAngle >= 360 || localSensorAngle <= -360) {localSensorAngle = 0;}
+
+        if (MXK_BlockSwitchTo(eMXK_Motor)) {
+                Motor_Move(&Stepper, degrees_to_steps(moveStepperTo));
+                if (MXK_Release())
+                        MXK_Dequeue();
+        }
+        //wait for the stepper to get to its position before calling something else
+        delay_ms(75);
 }
 
 //move the stepper to the forward direction of the irobot
 void move_stepper_neutral(void){
-    move_stepper_to(-localSensorAngle);
+        move_stepper_to(-localSensorAngle);
 }
 
 void irobot_move_to(INT16 moveToDistance, INT16 moveToSpeed){
-    INT16 reverseDistance;
-    distanceTotal = 0;
-    iRDistance = 0; iRVirtualWall = 0;
-	rightWall = 1; stepperMoved = 0;
-    while ((distanceTotal <= moveToDistance) && !iRVirtualWall){
-        follow_wall(moveToSpeed, get_apparentDistance(MAZE_CENTRE));
-        update_distance();
-        distanceTotal += iRDistance; 
-        update_virtual_wall();
-    }
-    irobot_stop_motion(0);
-    reverseDistance = -distanceTotal;
-    //if a v wall is detected, move back to the center from the cell in which it came
-    if (iRVirtualWall){
-        irobot_song_play(0);
-        move_stepper_neutral();
-        delay_ms(200);
-        irobot_move_to_straight(reverseDistance, -DEFAULT_SPEED);
-    }
-    stepperMoved = 0;
-    irobot_stop_motion(0);
+        INT16 reverseDistance;
+        distanceTotal = 0;
+        iRDistance = 0; iRVirtualWall = 0;
+        rightWall = 1; stepperMoved = 0;
+        while ((distanceTotal <= moveToDistance) && !iRVirtualWall) {
+                follow_wall(moveToSpeed, get_apparentDistance(MAZE_CENTRE));
+                update_distance();
+                distanceTotal += iRDistance;
+                update_virtual_wall();
+        }
+        irobot_stop_motion(0);
+        reverseDistance = -distanceTotal;
+        //if a v wall is detected, move back to the center from the cell in which it came
+        if (iRVirtualWall) {
+                irobot_song_play(0);
+                move_stepper_neutral();
+                delay_ms(200);
+                irobot_move_to_straight(reverseDistance, -DEFAULT_SPEED);
+        }
+        stepperMoved = 0;
+        irobot_stop_motion(0);
 }
 
 void irobot_move_to_straight(INT16 moveToDistance, INT16 moveToSpeed){
-    distanceTotal = 0;
-    iRDistance = 0;
-    irobot_move_straight(moveToSpeed);
-    if (moveToDistance > 0){
-        while (distanceTotal <= moveToDistance){
-            update_distance();
-            distanceTotal += iRDistance;              
+        distanceTotal = 0;
+        iRDistance = 0;
+        irobot_move_straight(moveToSpeed);
+        if (moveToDistance > 0) {
+                while (distanceTotal <= moveToDistance) {
+                        update_distance();
+                        distanceTotal += iRDistance;
+                }
         }
-    }
-    else{
-        while (distanceTotal >= moveToDistance){
-            update_distance();
-            distanceTotal += iRDistance;
+        else{
+                while (distanceTotal >= moveToDistance) {
+                        update_distance();
+                        distanceTotal += iRDistance;
+                }
         }
-    }
-    irobot_stop_motion(0);
+        irobot_stop_motion(0);
 }
 
 //moves a distance of one cell in the current forward facing direction of the robot
 void irobot_move_cell(void){
-    irobot_move_to(CELL_LENGTH, DEFAULT_SPEED);
-    move_stepper_neutral();
-    irobot_centralise();
+        irobot_move_to(CELL_LENGTH, DEFAULT_SPEED);
+        move_stepper_neutral();
+        irobot_centralise();
 }
 
 //-ve is CCW rotation, +ve is CW rotation
 void irobot_rotate_to(INT16 rotateToAngle, INT16 rotateToSpeed){
-    angleTotal = 0;
-    iRAngle = 0;
-    
-    irobot_rotate_continuous(rotateToAngle, rotateToSpeed);
-    
-    if (rotateToAngle > 0){
-        while (angleTotal >= -rotateToAngle) {
-            update_angle();
-            angleTotal += iRAngle;
+        angleTotal = 0;
+        iRAngle = 0;
+
+        irobot_rotate_continuous(rotateToAngle, rotateToSpeed);
+
+        if (rotateToAngle > 0) {
+                while (angleTotal >= -rotateToAngle) {
+                        update_angle();
+                        angleTotal += iRAngle;
+                }
         }
-    }
-    else{
-        while (angleTotal <= -rotateToAngle) {
-            update_angle();
-            angleTotal += iRAngle;
-        }        
-    }
-    irobot_stop_motion(0);
+        else{
+                while (angleTotal <= -rotateToAngle) {
+                        update_angle();
+                        angleTotal += iRAngle;
+                }
+        }
+        irobot_stop_motion(0);
 }
 
 // Basic movement functions for turning and going straight.
@@ -461,7 +464,7 @@ void printToConsole(){
         printf("%c", ENDOFTEXT);
         // Prints maze to console
         for (int i = 1; i < 10; i++) {
-                for (int j = 1;  j < 12; j++) {
+                for (int j = 1; j < 12; j++) {
                         currentPos = maze[i][j];
                         if (i == xPos && j == yPos) {
                                 currentPosChar = '*';
@@ -493,51 +496,51 @@ void printToConsole(){
 }
 
 void addVirtualWall(int i, int j){
-    maze[i][j] = 155;
+        maze[i][j] = 155;
 }
 
 void init(){
 //Program Initialisation
 ////////////////////////////////////////////////////////////////////////////////
 
-    //Init MXK Pins
-    MXK_Init();
-	ISR_Enable();
+        //Init MXK Pins
+        MXK_Init();
+        ISR_Enable();
 
-    //Init HMI
-    if (MXK_BlockSwitchTo(eMXK_HMI)) {
-        Console_Init();
-        HMI_Init();
-        LCD_Init();
-        if (MXK_Release())
-            MXK_Dequeue();
-    }
-	
-	//Init Motor
-	if (MXK_BlockSwitchTo(eMXK_Motor)) {
-		Motor_Init(&Stepper, MXK_MOTOR);
-		if (MXK_Release())
-			MXK_Dequeue();
-	}
-	
-	//Init IR
-	ADC_Init(&ADC_AN0, eADC_Ch0);
-	float ADC_Voltage(ADCPtr plnput);
-	ADC_Start(&ADC_AN0);
-	
-	//Init PIC EUSART and iRobot Comms
-    eusart_init();                                                              
-    irobot_init();   
-    irobot_led_power_on(0xA);
-    
-    irobot_init_song_0();
-    delay_ms(20);
+        //Init HMI
+        if (MXK_BlockSwitchTo(eMXK_HMI)) {
+                Console_Init();
+                HMI_Init();
+                LCD_Init();
+                if (MXK_Release())
+                        MXK_Dequeue();
+        }
+
+        //Init Motor
+        if (MXK_BlockSwitchTo(eMXK_Motor)) {
+                Motor_Init(&Stepper, MXK_MOTOR);
+                if (MXK_Release())
+                        MXK_Dequeue();
+        }
+
+        //Init IR
+        ADC_Init(&ADC_AN0, eADC_Ch0);
+        float ADC_Voltage(ADCPtr plnput);
+        ADC_Start(&ADC_AN0);
+
+        //Init PIC EUSART and iRobot Comms
+        eusart_init();
+        irobot_init();
+        irobot_led_power_on(0xA);
+
+        irobot_init_song_0();
+        delay_ms(20);
 }
 
 void main(){
-    init();	
-    loop(){
-        update_IR_distance();
-        update_SSD(IRValue);
-    }
+        init();
+        loop(){
+                update_IR_distance();
+                update_SSD(IRValue);
+        }
 }
